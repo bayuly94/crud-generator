7436a4918c2c5f3005de67f752a3b5b0b5fa14ea
Bayu Lukman Yusuf <bayuly94@gmail.com>
2024-09-09 13:37:22 +0700

    up

--- src/component/crud/searchbox.blade.php
+++ src/component/crud/searchbox.blade.php
@@ -1,10 +1,10 @@
 <form method="GET">
-    <div class="input-group">
-        <input type="search" class="form-control" placeholder="{{ __('Search') }}" aria-label="{{ __('Search') }}" name="search" value="{{ request()->search ?? '' }}">
-        <div class="input-group-append">
-          <button class="btn btn-outline-secondary" type="submit">
-            <i class="fas fa-search"></i>
-          </button>
-        </div>
-    </div>
+  <div class="input-group">
+      <input type="search" class="form-control" placeholder="{{ __('Search') }}" aria-label="{{ __('Search') }}" name="search" value="{{ request()->search ?? '' }}">
+      <div class="input-group-append">
+        <button class="btn btn-default" type="submit">
+          <i class="fas fa-search"></i>
+        </button>
+      </div>
+  </div>
 </form>
